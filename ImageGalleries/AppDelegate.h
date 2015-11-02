91f35ef13e79ee9375e79cc8f8df99fc0c1dc42f
keakalamudo <kelo.akalamudo@gmail.com>
2015-11-02 13:56:55 -0800

    Boilerplate

---
+++ ImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGalleries
+//
+//  Created by Kelo Akalamudo on 11/2/15.
+//  Copyright © 2015 Kelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
